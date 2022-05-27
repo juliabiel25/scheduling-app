785b40aeffc8764ff7a89bf834afd747db43d57c
Julia Bielecka <67067000+juliabiel25@users.noreply.github.com>
2022-05-27 20:24:07 +0200

    Rename components

---
+++ src/components/ScheduleNavigation.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../styles/ScheduleNavigation.css';
+import DateSelectionSet from '../utils/DateSelectionSet';
+import ScheduleTile from './ScheduleTile';
+
+export interface ScheduleNavigationProps {
+  schedule: DateSelectionSet[];
+  newSelectionSet: () => number;
+  getSelectionSetIndex: (id: number) => number;
+  focusedSelectionSet: number;
+  setFocusedSelectionSet: (index: number) => void;
+}
+
+const ScheduleNavigation: React.FC<ScheduleNavigationProps> = (props) => {
+  const selectionSets = props.schedule.map((selectionSet) => (
+    <ScheduleTile
+      key={selectionSet.id}
+      selectionSet={selectionSet}
+      getSelectionSetIndex={props.getSelectionSetIndex}
+      focusedSelectionSet={props.focusedSelectionSet}
+      setFocusedSelectionSet={props.setFocusedSelectionSet}
+    />
+  ));
+
+  return (
+    <div className="date-selection-navigation">
+      <button onClick={props.newSelectionSet}>+</button>
+      {selectionSets}
+    </div>
+  );
+};
+
+export default ScheduleNavigation;
